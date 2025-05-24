fedc3f13bc8e7bd713b8788844f5618e3c202a39
NavyaKoneri <konerinavya@gmail.com>
2025-05-24 10:34:21 +0530

    Add files via upload

---
+++ Alphabet.java
@@ -0,0 +1,15 @@
+class Alphabet 
+{
+	public static void main(String[] args) 
+	{
+		char ch='5';
+		if(ch>='A' && ch<='Z' || ch>='a' && ch<='z')
+		{
+		System.out.println(ch+" is a alphabet");
+		}
+		else
+		{
+		System.out.println(ch+" is not a alphabet");
+		}
+	}
+}
