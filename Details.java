ba5fa837d9d14665f4461eddee5dce7365c18f4e
NavyaKoneri <konerinavya@gmail.com>
2025-05-25 16:12:10 +0530

    Add files via upload

---
+++ Details.java
@@ -0,0 +1,22 @@
+class Details 
+{
+	public static void main(String[] args) 
+	{
+		String name="Navya";
+		int age=20;
+		double height=5.1;
+		char gender='F';
+		String gmailid="navya@gmail.com";
+		long mobileno=9848698858l;
+		String password="123@456";
+		long alternateno=8978531535l;
+		System.out.println(name);
+	    System.out.println(age);
+		System.out.println(height);
+		System.out.println(gender);
+		System.out.println(gmailid);
+		System.out.println(mobileno);
+		System.out.println(password);
+		System.out.println(alternateno);
+	}		
+}
