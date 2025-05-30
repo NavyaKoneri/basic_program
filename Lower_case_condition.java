160501b937d32eb9851d67214c3bedba691d513a
NavyaKoneri <konerinavya@gmail.com>
2025-05-30 19:23:13 +0530

    Add files via upload

---
+++ Lower_case_condition.java
@@ -0,0 +1,16 @@
+class Lower_case_condition 
+{
+	public static void main(String[] args) 
+	{ 
+		char ch='n';
+		if(ch>=97 && ch<=122)
+		{
+		 System.out.println(ch+" character is in lower case");
+		}
+		else
+		{
+		 System.out.println(ch+" character is in upper case");
+		}
+
+	}
+}
