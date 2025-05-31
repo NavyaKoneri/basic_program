3a4de413f9ba2b8006da0828ea0f48640d27d62c
NavyaKoneri <konerinavya@gmail.com>
2025-05-31 22:59:06 +0530

    Add files via upload

---
+++ Even_odd.java
@@ -0,0 +1,15 @@
+class Even_odd 
+{
+	public static void main(String[] args) 
+	{
+		int a=115;
+		if(a%2==0)
+		{
+		System.out.println(a+" is even number");
+		}
+		else
+		{
+		System.out.println(a+" is odd number");
+		}
+	}
+}
