519e289ad4a53c47a6c72264c784ce4ab0bd1030
NavyaKoneri <konerinavya@gmail.com>
2025-06-02 21:59:26 +0530

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,12 @@
+class Product 
+{
+	public static void main(String[] args) 
+	{
+		long product=1;
+		for(int i=10;i<=21;i++)
+		{
+			product=product*i;
+		}
+		System.out.println("The product of the range from 10 to 21 is :"+product);
+	}
+}
