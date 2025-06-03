8267be0f1fa77041643b81c0e4d2fa038b27a23a
NavyaKoneri <konerinavya@gmail.com>
2025-06-03 18:42:26 +0530

    Add files via upload

---
+++ Range_check2_condition.java
@@ -0,0 +1,16 @@
+class Range_check2_condition 
+{
+	public static void main(String[] args) 
+	{ 
+		char ch='v';
+		if(ch>='T' && ch<='Z')
+		{
+		 System.out.println(ch+" is in the range of 'T' to 'Z'");
+		}
+		else
+		{
+		 System.out.println(ch+" is not in the range of 'T' to 'Z'");
+		}
+
+	}
+}
