33fd467f2c497032f8e132c28048c8632e03a34f
NavyaKoneri <konerinavya@gmail.com>
2025-06-03 18:43:19 +0530

    Add files via upload

---
+++ Range_check3_condition.java
@@ -0,0 +1,16 @@
+class Range_check3_condition 
+{
+	public static void main(String[] args) 
+	{ 
+		char ch='3';
+		if(ch>='0' && ch<='9')
+		{
+		 System.out.println(ch+" given character is a digit");
+		}
+		else
+		{
+		 System.out.println(ch+" given character is not a digit");
+		}
+
+	}
+}
