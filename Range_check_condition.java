144f59b68e7a6a78b1e8441475512bed5dd75748
NavyaKoneri <konerinavya@gmail.com>
2025-06-03 18:40:37 +0530

    Add files via upload

---
+++ Range_check_condition.java
@@ -0,0 +1,17 @@
+class Range_check_condition 
+{
+	public static void main(String[] args) 
+	{ 
+		int a=75;
+		if(a>=10 && a<=50)
+		{
+		 System.out.println(a+" is in range of 10 to 50 ");
+		}
+		else
+		{
+		 System.out.println(a+" is not in range of 10 to 50");
+		}
+
+	}
+}
+
